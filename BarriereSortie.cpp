a0c25fb3928e7e603217c7764612928a4652f8ea
Losbar Thomas <tlosbar@iftpserv2.insa-lyon.fr>
2014-02-24 23:29:21 +0100

    tache mere opérationel avec simulation de fin sur kill -9 [processus de gererClavier]

---
+++ BarriereSortie.cpp
@@ -0,0 +1,40 @@
+/// ======================
+// INSA Lyon, Département Informatique
+// TP 3IF : Programmation concurrente
+// Binome : B3229
+// ======================
+
+//---------- Réalisation de la tâche <BarriereSortie> (fichier BarriereSortie.cpp) ---
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//-------------------------------------------------------- Include système
+
+//------------------------------------------------------ Include personnel
+#include "BarriereSortie.h"
+
+///////////////////////////////////////////////////////////////////  PRIVE
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//---------------------------------------------------- Variables statiques
+
+//------------------------------------------------------ Fonctions privées
+//static type nom ( liste de paramètres )
+// Mode d'emploi :
+//
+// Contrat :
+//
+// Algorithme :
+//
+//{
+//} //----- fin de nom
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
+//type Nom ( liste de paramètres )
+// Algorithme :
+//
+//{
+//} //----- fin de Nom
+
---
+++ BarriereSortie.h
@@ -0,0 +1,33 @@
+/// ======================
+// INSA Lyon, Département Informatique
+// TP 3IF : Programmation concurrente
+// Binome : B3229
+// ======================
+
+//---------- Interface de la tâche <BarriereSortie> (fichier BarriereSortie.h) -------
+#if ! defined ( BARRIERESORTIE_H )
+#define BARRIERESORTIE_H
+
+//------------------------------------------------------------------------
+// Rôle de la tâche <BarriereSortie>
+//
+//
+//------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
+// type Nom ( liste de paramètres );
+// Mode d'emploi :
+//
+// Contrat :
+//
+
+#endif // BARRIERESORTIE_H
+
